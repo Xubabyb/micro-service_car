a21eb57f9c0daad47a5f24a3b124e0dada2de0de
DmitriyShidlouskij <xubabyb@gmail.com>
2022-08-09 08:22:43 +0300

    first commit

---
+++ src/main/java/my/home/service_car/ServiceCarApplication.java
@@ -0,0 +1,13 @@
+package my.home.service_car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceCarApplication.class, args);
+    }
+
+}
